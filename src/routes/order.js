7cae3c29775396159f48ab49482937d19dd4ab00
Ishika Patel <ishikapatel1364@gmail.com>
2026-05-26 12:37:29 +0530

    add: cancelOrder route with authentication

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createOrder } from '../controllers/order.js';
+import { cancelOrder, createOrder } from '../controllers/order.js';
 import { protect } from '../middlewares/auth.js';
 
 const router = Router();
 
 router.post('/', protect(), createOrder);
+router.delete('/:id', protect(), cancelOrder);
 
 export default router;
